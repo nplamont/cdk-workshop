9d1141597a31aec0204934896440c36558b0ff93
Neil Lamomnt <neil.lamont@rackspace.co.uk>
2021-06-13 21:26:36 +0100

    Initial workshop files

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,9 +1,6 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda,
 )
 
 class CdkworkshopStack(core.Stack):
@@ -11,13 +8,9 @@ class CdkworkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
         )
-
-        topic = sns.Topic(
-            self, "CdkworkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
